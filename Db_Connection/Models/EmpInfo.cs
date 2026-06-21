160e9ad77b75cb145bda8a961b9363383fa2153f
sujit kumar <sujitkumar93042@gmail.com>
2026-06-21 12:21:20 +0530

    new changes

---
+++ Db_Connection/Models/EmpInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Connection.Models
+{
+    internal class EmpInfo
+    {
+        public string Name { get; set; }
+        public string Address { get; set; } 
+        public string Email { get; set; }
+    }
+}
